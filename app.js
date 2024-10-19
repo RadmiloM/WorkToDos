cfb3ed0fa633b61cbadf45846956730f93dcaeae
RadmiloM <markovicradmilo35@gmail.com>
2024-10-19 16:48:21 +0200

    added ul element with list item elements

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const button = document.querySelector('button');
 const userInput = document.querySelector('input');
+const ulContainer = document.createElement('ul');
+document.body.appendChild(ulContainer);
+
 button.addEventListener('click', () => {
-    console.log(userInput.value);
+    const userInputValue = userInput.value;
+    const listItem = document.createElement('li');
+    listItem.textContent = userInputValue;
+    ulContainer.appendChild(listItem);
 })
