ebcda9543b66365a7e625302c9a336b093dff8fc
RadmiloM <markovicradmilo35@gmail.com>
2024-10-19 19:13:34 +0200

    added flex-container and flex-column classes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const button = document.querySelector('button');
 const userInput = document.querySelector('input');
 const ulContainer = document.createElement('ul');
+ulContainer.classList = 'flex-container flex-column';
 document.body.appendChild(ulContainer);
 
 button.addEventListener('click', () => {
